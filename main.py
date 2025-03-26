ef9086af5babaaa269c0565ebbd46110954934db
Benjamin <vanix69yt@gmail.com>
2025-03-26 12:02:19 +0000

    Removed sensitive token

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class CryptoPredictionSystem:
         # API keys are now optional since we use free APIs
         self.news_api_key = None  # No longer required
         self.crypto_api_key = None  # No longer required
-        self.discord_token = 'MTI2MzA2MzIzMTkyMDg2OTM5OQ.GiI_OP.Ae1C1SLS_CS3GUom1BfiozzNgdYIZqcja84Euo'  # Only Discord token might be needed
+        self.discord_token = ''  # Only Discord token might be needed
         
         # Create data directories
         os.makedirs('data', exist_ok=True)
